9f32e4fea0ad6ca4df40cee683c33afeb7368c9e
Diamantino-Op <Diamantino.op@gmail.com>
2022-10-02 13:32:34 +0200

    Chaneges

--- src/main/java/com/diamantino/stevevsalex/entities/base/ProjectileEntity.java
+++ src/main/java/com/diamantino/stevevsalex/entities/base/ProjectileEntity.java
@@ -4,6 +4,7 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.HitResult;
 
 public class ProjectileEntity extends AbstractArrow {
     protected ProjectileEntity(EntityType<? extends ProjectileEntity> p_36721_, Level p_36722_) {
@@ -12,6 +13,11 @@ public class ProjectileEntity extends AbstractArrow {
 
     public ProjectileEntity(EntityType<? extends ProjectileEntity> p_36721_, Level p_36861_, double p_36862_, double p_36863_, double p_36864_) {
         super(p_36721_, p_36862_, p_36863_, p_36864_, p_36861_);
+    }
+
+    @Override
+    protected void onHit(HitResult p_37260_) {
+        kill();
     }
 
     @Override
